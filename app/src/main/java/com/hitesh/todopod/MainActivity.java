68e7020c37cae4c18543f170a73aba03349fd558
hiteshhv <46933160+hiteshhv@users.noreply.github.com>
2020-04-26 13:57:02 +0530

    Remove unused imports

--- app/src/main/java/com/hitesh/todopod/MainActivity.java
+++ app/src/main/java/com/hitesh/todopod/MainActivity.java
@@ -3,10 +3,8 @@ package com.hitesh.todopod;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
