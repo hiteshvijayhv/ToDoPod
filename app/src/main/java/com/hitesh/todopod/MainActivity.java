a69d5261b8e6b4c0e9427316c95726bc83a7b72b
hiteshhv <46933160+hiteshhv@users.noreply.github.com>
2020-05-08 20:58:08 +0530

    Fix app Force stop

--- app/src/main/java/com/hitesh/todopod/MainActivity.java
+++ app/src/main/java/com/hitesh/todopod/MainActivity.java
@@ -8,11 +8,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -66,6 +63,9 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+
+
 
         /*listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
