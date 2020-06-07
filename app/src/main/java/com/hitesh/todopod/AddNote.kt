7826be5bb45a4ef04298b9b919f82b2221b686a6
hiteshhv <46933160+hiteshhv@users.noreply.github.com>
2020-06-07 13:54:39 +0530

    Move AddNoteActivity to Kotlin

---
+++ app/src/main/java/com/hitesh/todopod/AddNote.kt
@@ -0,0 +1,29 @@
+package com.hitesh.todopod
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
+class AddNote : AppCompatActivity() {
+    var editText: EditText? = null
+    var saveButton: Button? = null
+    var userInput: String? = null
+    var db: DatabaseHelper? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_note)
+        editText = findViewById<View>(R.id.editText) as EditText
+        saveButton = findViewById<View>(R.id.saveButton) as Button
+        db = DatabaseHelper(this)
+    }
+
+    fun saveNote(view: View?) {
+        userInput = editText!!.text.toString()
+        db!!.insertData(userInput)
+        val mainActivity = Intent(applicationContext, MainActivity::class.java)
+        startActivity(mainActivity)
+    }
+}
