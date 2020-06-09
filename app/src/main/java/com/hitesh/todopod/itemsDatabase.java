8f1d91fc0cb49ca6589ba392726c7c18325571c1
hiteshhv <46933160+hiteshhv@users.noreply.github.com>
2020-06-09 18:19:53 +0530

    Delete

--- app/src/main/java/com/hitesh/todopod/itemsDatabase.java
+++ app/src/main/java/com/hitesh/todopod/itemsDatabase.java
@@ -37,9 +37,6 @@ public abstract class itemsDatabase extends RoomDatabase {
         }
         @Override
         protected Void doInBackground(Void... voids) {
-            itemsDao.insert(new items("Title 1", "Description 1", 1));
-            itemsDao.insert(new items("Title 2", "Description 2", 2));
-            itemsDao.insert(new items("Title 3", "Description 3", 3));
             return null;
         }
     }
