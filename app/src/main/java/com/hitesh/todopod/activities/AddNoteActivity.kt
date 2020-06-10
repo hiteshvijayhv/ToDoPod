624a25f7c94a3ae23119058535950f8d0a038c03
hiteshhv <46933160+hiteshhv@users.noreply.github.com>
2020-06-10 10:40:04 +0530

    Save note from AddNoteActivity

--- app/src/main/java/com/hitesh/todopod/activities/AddNoteActivity.kt
+++ app/src/main/java/com/hitesh/todopod/activities/AddNoteActivity.kt
@@ -6,25 +6,31 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.hitesh.todopod.ItemsViewModel
 import com.hitesh.todopod.helper.DatabaseHelper
 import com.hitesh.todopod.R
+import com.hitesh.todopod.items
 
 class AddNoteActivity : AppCompatActivity() {
     var editText: EditText? = null
     var saveButton: Button? = null
     var userInput: String? = null
-    var db: DatabaseHelper? = null
+    var itemsViewModel: ItemsViewModel? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_note)
         editText = findViewById<View>(R.id.editText) as EditText
         saveButton = findViewById<View>(R.id.saveButton) as Button
-        //db = DatabaseHelper(this)
+        itemsViewModel = ViewModelProvider(this,
+                ViewModelProvider.AndroidViewModelFactory(application))
+                .get(ItemsViewModel::class.java)
     }
 
     fun saveNote(view: View?) {
-        userInput = editText!!.text.toString()
-        db!!.insertData(userInput)
+        userInput = editText?.text.toString()
+        val input = items("" + userInput, "", 0)
+        itemsViewModel?.insert(input)
         val mainActivity = Intent(applicationContext, MainActivity::class.java)
         startActivity(mainActivity)
     }
--- app/src/main/java/com/hitesh/todopod/activities/MainActivity.kt
+++ app/src/main/java/com/hitesh/todopod/activities/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.hitesh.todopod.activities
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -15,17 +18,25 @@ import com.hitesh.todopod.ItemsViewModel
 import com.hitesh.todopod.R
 import com.hitesh.todopod.adapter.RecyclerViewAdapter
 import com.hitesh.todopod.items
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
+
 class MainActivity : AppCompatActivity() {
+
     private val itemsList: List<items> = ArrayList()
     private var recyclerView: RecyclerView? = null
     private var mAdapter: RecyclerViewAdapter? = null
     var itemsViewModel: ItemsViewModel? = null
 
+    var isFABOpen = false
+    var fabLayout1: LinearLayout? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fabLayout1 = findViewById(R.id.fabLayout1)
 
         itemsViewModel = ViewModelProvider(this,
                 AndroidViewModelFactory(application))
@@ -39,12 +50,43 @@ class MainActivity : AppCompatActivity() {
         recyclerView?.adapter = mAdapter
 
         itemsViewModel?.allitemss?.observe(this, Observer { items -> mAdapter?.setNotes(items!!) })
+
         val editText = findViewById<EditText>(R.id.editText)
         val button = findViewById<Button>(R.id.button)
+
         button.setOnClickListener {
             val text = editText.text.toString()
             val item1 = items("" + text, "", 0)
             itemsViewModel?.insert(item1)
         }
+        fab.setOnClickListener{
+            if(!isFABOpen){
+                open();
+                fabLayout1?.setVisibility(View.VISIBLE);
+                isFABOpen = true;
+            } else {
+                close();
+                isFABOpen = false;
+            }
+            Toast.makeText(applicationContext, "Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        fab1.setOnClickListener {
+            Toast.makeText(applicationContext, "clicked", Toast.LENGTH_SHORT).show()
+            val addNote = Intent(applicationContext, AddNoteActivity::class.java)
+            startActivity(addNote)
+        }
     }
+    fun open() {
+        fabLayout1!!.animate().translationY(-50f)
+        fab.animate().rotationBy(135f)
+        fabLayout1!!.animate().alpha(1f).duration = 500
+    }
+
+    fun close() {
+        fabLayout1!!.animate().translationY(10f)
+        fab.animate().rotationBy(-135f)
+        fabLayout1!!.animate().alpha(0.0f).duration = 500
+    }
+
 }
